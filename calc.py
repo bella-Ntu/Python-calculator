5628f17206a7953a776973a8563726fb2d25edce
bella-Ntu <isabellantuha3@gmail.com>
2026-04-05 21:18:13 +0300

    Create calc.py
    update

---
+++ calc.py
@@ -0,0 +1,33 @@
+def calculator(number):
+    
+    while True:
+        numbers = input('Enter number' + str(number) + ':')
+        try:
+            return float(numbers)
+        except ValueError:
+            print('Invalid number entered. Try again')
+num1 = calculator(1)       
+num2 = calculator(2)
+
+sign  = input('Enter the sign + / - * %: ')
+print(f'{num1} {sign} {num2}')
+
+result = 0
+if sign == '+':
+    result = num1 + num2
+elif sign == '-':
+    result = num1 - num2       
+elif sign == '*':
+    result = num1 * num2
+elif sign == '/':
+    if num2 != 0:
+        result = num1 / num2 
+    else:
+        print('Division by zero error')
+elif sign == '%':
+    result = num1 % num2                
+else:
+    print('Enter a avlid sign')
+
+print(result)                                       
+                                      
